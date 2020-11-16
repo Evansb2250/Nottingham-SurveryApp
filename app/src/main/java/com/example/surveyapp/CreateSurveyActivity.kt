55e7be45d62eeb5189ad88a9848306264a44753d
Evansb2250 <samuelebrandenburg12@gmail.com>
2020-11-16 14:13:05 +0000

    fixed SOR bug/
    Signed-off-by: Evansb2250 <samuelebrandenburg12@gmail.com>

--- app/src/main/java/com/example/surveyapp/CreateSurveyActivity.kt
+++ app/src/main/java/com/example/surveyapp/CreateSurveyActivity.kt
@@ -1,8 +1,12 @@
 package com.example.surveyapp
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
 
 class CreateSurveyActivity : AppCompatActivity() {
@@ -10,6 +14,19 @@ class CreateSurveyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_survey)
 
+
+        val button = findViewById<Button>(R.id.nextButton_CS)
+        button.setOnClickListener({ v ->
+            changeFrame()
+        })
     }
 
+
+    fun changeFrame() {
+        val intent = Intent(this, SORActivity::class.java)
+        startActivity(intent)
+
+    }
 }
+
+
