791d7e9928410f1866074ee40534dc29ddf6d844
Evansb2250 <samuelebrandenburg12@gmail.com>
2020-12-09 11:15:08 +0000

    created data class Locks
    Signed-off-by: Evansb2250 <samuelebrandenburg12@gmail.com>

---
+++ app/src/main/java/com/example/surveyapp/database/Locks.kt
@@ -0,0 +1,11 @@
+package com.example.surveyapp.database
+
+data class Locks(
+    var lockID: Int,
+    var numOfInsuranceFit: Int,
+    var numOfInsuranceLockFit: Int,
+    var numOfCylinderLockRenew: Int,
+    var numOfFixOnlyLock: Int,
+    var numOfDoubleGlazed50: Int,
+    var numOfDouleGlazed1p9: Int,
+)
