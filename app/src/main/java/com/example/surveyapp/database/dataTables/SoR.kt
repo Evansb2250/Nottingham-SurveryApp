477c26c87a23b406f97e09a446875c5d1c50305b
Evansb2250 <samuelebrandenburg12@gmail.com>
2020-12-17 14:40:27 +0000

    restruct code
    Signed-off-by: Evansb2250 <samuelebrandenburg12@gmail.com>

--- app/src/main/java/com/example/surveyapp/database/dataTables/SoR.kt
+++ app/src/main/java/com/example/surveyapp/database/dataTables/SoR.kt
@@ -8,7 +8,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "SoR_table")
 data class SoR(
     @PrimaryKey val sorCode: String,
-    @ColumnInfo(name = "Category") val category: String,
     @ColumnInfo(name = "Description") val description: String,
     @ColumnInfo(name = "Recharge_Price") val rechargeRate: Double
 )
