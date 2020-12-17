adabd26e1fe863e14b1c4e8ef028ff8fef93b359
Evansb2250 <samuelebrandenburg12@gmail.com>
2020-12-17 16:13:08 +0000

    added database
    Signed-off-by: Evansb2250 <samuelebrandenburg12@gmail.com>

--- app/src/main/java/com/example/surveyapp/database/dataTables/SoRDatabaseDao.kt
+++ app/src/main/java/com/example/surveyapp/database/dataTables/SoRDatabaseDao.kt
@@ -1,8 +1,11 @@
 package com.example.surveyapp.database.dataTables
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
+@Dao
 interface SoRDatabaseDao {
     @Insert
     fun insert(sor: SoR)
---
+++ app/src/main/java/com/example/surveyapp/database/dataTables/SurveyDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.surveyapp.database.dataTables
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SoR::class], version = 1, exportSchema = false)
+abstract class SurveyDatabase : RoomDatabase() {
+
+    //make the SurveyDatabse know about the Dao
+    abstract var sorDatabaseDao: SoRDatabaseDao?
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SurveyDatabase? = null
+
+        fun getInstance(context: Context): SurveyDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SurveyDatabase::class.java, "Suvery_History_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+
+    }
+
+}
+
