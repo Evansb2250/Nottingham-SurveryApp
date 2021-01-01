8285323b11b01b3c58a50bc03f1aa603ca979a6e
Evansb2250 <samuelebrandenburg12@gmail.com>
2021-01-01 18:10:34 +0000

    Finished all Functionality beside writing textFile
    Signed-off-by: Evansb2250 <samuelebrandenburg12@gmail.com>

--- app/src/main/java/com/example/surveyapp/fragments/confirmationTab/ConfirmationPage.kt
+++ app/src/main/java/com/example/surveyapp/fragments/confirmationTab/ConfirmationPage.kt
@@ -1,12 +1,8 @@
 package com.example.surveyapp.fragments.confirmationTab
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import androidx.fragment.app.Fragment
@@ -15,20 +11,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat
-import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.databinding.DataBindingUtil
 import com.example.surveyapp.R
-import com.example.surveyapp.activities.PDF
 import com.example.surveyapp.activities.SurveyActivity
 import com.example.surveyapp.databinding.FragmentConfirmationPageBinding
+
 import com.example.surveyapp.domains.SurveySORs
-import com.itextpdf.text.Document
-import com.itextpdf.text.Paragraph
-import com.itextpdf.text.pdf.PdfWriter
-import java.io.FileOutputStream
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import java.io.*
+
 import java.text.DecimalFormat
-import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -39,12 +32,11 @@ import java.util.*
  */
 class ConfirmationPage : Fragment() {
 
-    private val STORAGE_CODE: Int = 100
-    private val currency = DecimalFormat("£###,###.##")
-    private val numberCounter = DecimalFormat("####. ")
+
     lateinit var binding: FragmentConfirmationPageBinding
 
 
+    @SuppressLint("WrongThread")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -53,12 +45,29 @@ class ConfirmationPage : Fragment() {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_confirmation_page, container, false)
         binding.textField.isFocusable = false
+        val currency = DecimalFormat("£###,###.##")
+
+        binding.viewModel = SurveyActivity.confirmPage
+        binding.lifecycleOwner = this
 
         buttonListener(binding.button2)
         setUpPdfButton(binding.button4)
+        setUpCancelButton(binding.cancelButton)
 
+//
+//        SurveyActivity.confirmPage?.DataFromSurvey?.observe(viewLifecycleOwner, { list ->
+//            Toast.makeText(requireContext(), "Observer act", Toast.LENGTH_LONG).show()
+//            SurveyActivity.confirmPage!!.updateTotal(list)
+//        })
 
+        SurveyActivity.confirmPage?.total?.observe(viewLifecycleOwner, { newAmount ->
+            binding.total.text = currency.format(newAmount)
+        })
 
+        SurveyActivity.confirmPage?.message?.observe(viewLifecycleOwner, { newMessage ->
+            binding.textField.text.clear()
+            binding.textField.setText(newMessage)
+        })
 
 
 
@@ -67,18 +76,36 @@ class ConfirmationPage : Fragment() {
     }
 
 
+    private fun setUpCancelButton(cancelButton: Button) {
+        cancelButton.setOnClickListener { it ->
+            activity?.finish()
+
+        }
+    }
+
+
     fun setUpPdfButton(pdfButton: Button) {
         pdfButton.setOnClickListener { it ->
+            try {
 
+                val filename = "myfile"
+                val fileContents = "Hello world!"
+                val file = File(context?.filesDir, filename)
+                context?.openFileOutput(filename, Context.MODE_PRIVATE).use {
+                    if (it != null) {
+                        it.write(fileContents.toByteArray())
+                        it.close()
+                    }
+                }
 
-            val intent = Intent(activity, PDF::class.java)
-            var text = binding.textField.text.toString()
-
-            val hello = "Hello"
-            intent.putExtra("surveyInfo", text)
-            intent.putExtra("test", hello)
-            requireActivity().startActivity(intent)
-
+                Toast.makeText(requireContext(), "File Created", Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) {
+                Toast.makeText(
+                    requireContext(),
+                    "ExceptionFile write failed: " + e.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
@@ -86,30 +113,12 @@ class ConfirmationPage : Fragment() {
     private fun buttonListener(button2: Button) {
 
         button2.setOnClickListener { it ->
-            var dataFromSor = SurveyActivity.sorViewModel!!.returnListSORLIST()
-            var dataFromPrev = SurveyActivity.prevViewModel!!.returnPreviosWorkData()
-            var experimentunite = combineList(dataFromSor, dataFromPrev)
-
-            Toast.makeText(requireContext(), experimentunite.size.toString(), Toast.LENGTH_SHORT)
-                .show()
-            binding.textField.text.clear()
-            var total: Double = 0.0
-            var header = "\t\t\tSoR\t\t\t\t\t\t\t\tQTY\t\t\t\t\t\tTotal+\t\t\t\t\t\t\t\tComments\n"
-            var dataString: String = ""
-            var count = 1
-            for (sor in experimentunite) {
-                dataString += (numberCounter.format(count) + sor.sorCode + "\t\t\t\t\t\t" + sor.sorDescription + "\t\t\t\t\t\t" + sor.UOM + "\t\t\t\t\t\t" + sor.quantity.toString() + "\t\t\t\t\t\t" + currency.format(
-
-                    sor.total
-                ) + "\t\t\t\t\t\t\t\t\t\t" + sor.surveyorDescription + "\n")
-                count += 1
-                total += sor.total
-//            }
-
-                binding.textField.setText(header + dataString)
-                binding.total.text = currency.format(total)
-
-            }
+            Toast.makeText(requireContext(), "Loading.....", Toast.LENGTH_SHORT).show()
+            val list = SurveyActivity.confirmPage?.getData()
+            if (list != null) {
+                SurveyActivity.confirmPage!!.updateTotal(list)
+            } else
+                Toast.makeText(requireContext(), "Error Occurred!!.", Toast.LENGTH_LONG).show()
 
 
         }
@@ -119,21 +128,6 @@ class ConfirmationPage : Fragment() {
 
 }
 
-
-fun combineList(dataFromSor: List<SurveySORs>, dataFromPrev: List<SurveySORs>): List<SurveySORs> {
-    var list = mutableListOf<SurveySORs>()
-
-
-    for (data in dataFromSor) {
-        list.add(data)
-    }
-    for (data in dataFromPrev) {
-        list.add(data)
-    }
-
-
-    return list
-}
 
 
 
