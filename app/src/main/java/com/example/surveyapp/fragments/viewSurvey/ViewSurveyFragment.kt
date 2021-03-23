559fed4b68ce7ed1e77dd9b2bda91588fc24cb4a
samue <samuelebrandenburg12@gmail.com>
2021-03-22 19:18:10 -0500

    changed viewSurvey fragment and layout

--- app/src/main/java/com/example/surveyapp/fragments/viewSurvey/ViewSurveyFragment.kt
+++ app/src/main/java/com/example/surveyapp/fragments/viewSurvey/ViewSurveyFragment.kt
@@ -16,13 +16,14 @@ import com.example.surveyapp.CONSTANTS.constant
 import com.example.surveyapp.R
 import com.example.surveyapp.activities.SurveyActivity
 import com.example.surveyapp.application.SurveyApplication
-import com.example.surveyapp.databinding.FragmentViewSurveyBinding
+import com.example.surveyapp.databinding.FragmentViewSurvey2Binding
+
 import java.text.DecimalFormat
 
 
 class ViewSurveyFragment : Fragment() {
 
-    private lateinit var binding: FragmentViewSurveyBinding
+    private lateinit var binding: FragmentViewSurvey2Binding
     private val currency = DecimalFormat("£###,###.##")
 
     private val viewModel: ViewSurveyViewModel by viewModels {
@@ -35,9 +36,10 @@ class ViewSurveyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_view_survey, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_view_survey2, container, false)
 
-
+//
 
         binding.imageButtonRequestSearch.setOnClickListener {
             val searchMethod = binding.searchBySpinner.getSelectedItem()
@@ -45,29 +47,23 @@ class ViewSurveyFragment : Fragment() {
             viewModel.requestSurvey(searchMethod, surveyDetail)
 
         }
+//
+        binding.deleteSurveyButton.setOnClickListener {
+            if (viewModel.currentSurvey.value != null) {
 
-        binding.deleteSurveyButton.setOnClickListener{
-            if(viewModel.currentSurvey.value != null){
-
-              // Toast.makeText(requireContext(), viewModel.position.toString(), Toast.LENGTH_LONG).show()
-                  viewModel.deleteSurvey()
-                 clearDescriptionField()
-            }else
+                // Toast.makeText(requireContext(), viewModel.position.toString(), Toast.LENGTH_LONG).show()
+                viewModel.deleteSurvey()
+                clearDescriptionField()
+            } else
                 Toast.makeText(requireContext(), "Must select a survey", Toast.LENGTH_LONG).show()
-                resetSearchField()
+            resetSearchField()
         }
-
-
-
-
-
-
 
         viewModel.id.observe(viewLifecycleOwner, Observer { id ->
             Toast.makeText(requireContext(), "Last ID " + id.toString(), Toast.LENGTH_LONG).show()
         })
-
-
+//
+//
         viewModel.validResponse.observe(viewLifecycleOwner, Observer { flag ->
             if (!flag) {
                 Toast.makeText(requireContext(), viewModel.errorMessage, Toast.LENGTH_LONG).show()
@@ -75,27 +71,51 @@ class ViewSurveyFragment : Fragment() {
             resetSearchField()
         })
 
+
+
         viewModel.currentSurvey.observe(viewLifecycleOwner, Observer { survey ->
             if (survey != null) {
-                val details =
-                    " Survey ID: ${survey.surveyId} \n\n Name: ${survey.surveyorName}\n\n Date: ${survey.day}/${survey.month}/${survey.year}\n\n" +
-                            " Survey Type: ${survey.surveyType}\n\n Address: ${survey.address}\n\n Post Code: ${survey.postCode}\n\n " +
-                            "Survey Total: ${currency.format(survey.surveyTotal)}\n\n Recharge Total: ${
-                                currency.format(
-                                    survey.rechargeTotal
-                                )
-                            } "
+                binding.surveyIDEdit.setText(survey.surveyId.toString())
+                binding.nameEdit.setText(survey.surveyorName)
+                binding.dateEdit.setText("${survey.day}/${survey.month}/${survey.year}")
+                binding.surveyTypeEdit.setText(survey.surveyType)
+                binding.addressEdit.setText(survey.address)
+                binding.postCodeEdit.setText(survey.postCode)
+                binding.surveyTotalEdit.setText(currency.format(survey.surveyTotal))
+                binding.rechargeAmountEdit.setText(currency.format(survey.rechargeTotal))
 
-                binding.descriptionBox.setText(details)
             } else
-                binding.descriptionBox.setText("")
+                clearDescriptionField()
 
         })
+
+
+
         binding.listView.setOnItemClickListener { parent, view, position, id ->
             viewModel.changeCurrentSurvey(position)
         }
 
-
+//        viewModel.currentSurvey.observe(viewLifecycleOwner, Observer { survey ->
+//            if (survey != null) {
+//                val details =
+//                    " Survey ID: ${survey.surveyId} \n\n Name: ${survey.surveyorName}\n\n Date: ${survey.day}/${survey.month}/${survey.year}\n\n" +
+//                            " Survey Type: ${survey.surveyType}\n\n Address: ${survey.address}\n\n Post Code: ${survey.postCode}\n\n " +
+//                            "Survey Total: ${currency.format(survey.surveyTotal)}\n\n Recharge Total: ${
+//                                currency.format(
+//                                    survey.rechargeTotal
+//                                )
+//                            } "
+//
+//                binding.descriptionBox.setText(details)
+//            } else
+//                binding.descriptionBox.setText("")
+//
+//        })
+//        binding.listView.setOnItemClickListener { parent, view, position, id ->
+//            viewModel.changeCurrentSurvey(position)
+//        }
+//
+//
 
         viewModel.changeDetected.observe(viewLifecycleOwner, Observer { flag ->
             setUpListView(viewModel.listOfSurveyTitles)
@@ -122,15 +142,22 @@ class ViewSurveyFragment : Fragment() {
         }
 
 
-
-
         return binding.root
     }
 
+    //
     private fun clearDescriptionField() {
-        binding.descriptionBox.setText("")
+        binding.surveyIDEdit.setText("")
+        binding.nameEdit.setText("")
+        binding.dateEdit.setText("")
+        binding.surveyTypeEdit.setText("")
+        binding.addressEdit.setText("")
+        binding.postCodeEdit.setText("")
+        binding.surveyTotalEdit.setText("")
+        binding.rechargeAmountEdit.setText("")
     }
 
+    //
     private fun resetSearchField() {
         binding.searchEditField.setText("")
     }
